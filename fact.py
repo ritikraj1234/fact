c1b93d498ecf27beea669c7e51650793dbfdd50e
ritikraj1234 <mayankroy153@gmail.com>
2024-08-09 16:54:08 +0530

    second

--- fact.py
+++ fact.py
@@ -1,8 +1,8 @@
-n = 15
+n = 4
 fact = 1
 
 for i in range(1,n+1):
- fact = fact *I
+ fact = fact *i
 
-print("The factorial of 15 is :, end= "")
+print("The factorial of 15 is :", fact)
 print(fact)
